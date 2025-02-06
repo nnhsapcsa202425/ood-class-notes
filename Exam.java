fd3d231fcf6714a2167b55cf7af323376ff987b9
Matt Callaghan <mcallaghan@naperville203.org>
2025-02-06 13:55:53 -0600

    Finished notes for question class

--- Exam.java
+++ Exam.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * An exam *has a* list of questions.
@@ -18,4 +19,23 @@ public class Exam
         this.questions = new ArrayList<Question>();
     }
     
+    public void addQuestion(Question q)
+    {
+        this.questions.add(q);
+    }
+    
+    public void askQuestions()
+    {
+        Scanner in = new Scanner(System.in);
+        for(Question q : questions)
+        {
+            System.out.println(q.toString());
+            
+            System.out.print("Your answer: ");
+            String response = in.nextLine();
+            
+            System.out.println(q.checkAnswer(response));
+        }
+        
+    }
 }
--- QuestionDemo.java
+++ QuestionDemo.java
@@ -11,28 +11,37 @@ public class QuestionDemo
 
       Question q = new FillInQuestion("The inventor of Java is _James Gosling_.");
       //q.setText("The inventor of Java is _James Gosling_.");
-
+      
+      /*
       System.out.println( q );
       System.out.print("Your answer: ");
       String response = in.nextLine();
       System.out.println(q.checkAnswer(response));
-      
+      */
       
       FillInQuestion q2 = new FillInQuestion("The IDE we use in APCSA is _BlueJ_.");
-      
+      /*
       System.out.println( q2 );
       System.out.print("Your answer: ");
       String response2 = in.nextLine();
       System.out.println(q2.checkAnswer(response2));
-      
+      */
       ChoiceQuestion q3 = new ChoiceQuestion("Who founded Apple computers?");  
       q3.addChoice("Bill Gates", false);
       q3.addChoice("Steve Jobs", true);
-      
+      /*
       System.out.println( q3 );
       System.out.print("Your answer: ");
       String response3 = in.nextLine();
       System.out.println(q3.checkAnswer(response3));
+      */
+     
+      Exam exam = new Exam();
+      exam.addQuestion(q);
+      exam.addQuestion(q2);
+      exam.addQuestion(q3);
+      exam.askQuestions();
+      
       
       
    }
