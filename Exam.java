00964c55b4f1965aee09a0a266d8dc09270863ad
Matt Callaghan <mcallaghan@naperville203.org>
2025-02-07 07:29:04 -0600

    Finished inheritance notes

--- Exam.java
+++ Exam.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * An exam *has a* list of questions.
  *
@@ -11,4 +13,30 @@ public class Exam
      * An exam object *has a* list of questions; an exam *is not* a question
      */
     private ArrayList<Question> questions;
+    
+    public Exam()
+    {
+        this.questions = new ArrayList<Question>();
+    }
+    
+    public void addQuestion(Question q)
+    {
+        this.questions.add(q);
+    }
+    
+    public void askQuestions()
+    {
+        Scanner in = new Scanner(System.in);
+        
+        for(Question q : questions)
+        {
+            System.out.println(q.toString());
+            System.out.print("Your answer: ");
+            String response = in.nextLine();
+            System.out.println(q.checkAnswer(response));
+        }
+        
+    }
+    
+    
 }
--- QuestionDemo.java
+++ QuestionDemo.java
@@ -1,42 +1,48 @@
 import java.util.Scanner;
 
 /**
-   This program shows a simple quiz with one question.
-*/
+This program shows a simple quiz with one question.
+ */
 public class QuestionDemo
 {
-   public static void main(String[] args)
-   {
-      Scanner in = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
 
-      Question q = new FillInQuestion("The inventor of Java is _James Gosling_.");
-      //q.setText("The inventor of Java is _James Gosling_.");
+        Question q = new FillInQuestion("The inventor of Java is _James Gosling_.");
+        //q.setText("The inventor of Java is _James Gosling_.");
+        /*
+        System.out.println( q );
+        System.out.print("Your answer: ");
+        String response = in.nextLine();
+        System.out.println(q.checkAnswer(response));
+         */
+        // question 2
+        FillInQuestion q2 = new FillInQuestion("The IDE for APCSA is _BlueJ_.");
+        //q2.setText("The IDE for APCSA is _BlueJ_.");
+        /*
+        System.out.println( q2 );
+        System.out.print("Your answer: ");
+        String response2 = in.nextLine();
+        System.out.println(q2.checkAnswer(response2));
+         */
+        ChoiceQuestion q3 = new ChoiceQuestion("Who founded Apple?");
+        q3.addChoice("Bill Gates", false);
+        q3.addChoice("Steve Jobs", true);
+        /*
+        System.out.println( q3.toString() );
+        System.out.print("Your answer: ");
+        String response3 = in.nextLine();
+        System.out.println(q3.checkAnswer(response3));
+         */
 
-      System.out.println( q );
-      System.out.print("Your answer: ");
-      String response = in.nextLine();
-      System.out.println(q.checkAnswer(response));
-      
-      // question 2
-      FillInQuestion q2 = new FillInQuestion("The IDE for APCSA is _BlueJ_.");
-      //q2.setText("The IDE for APCSA is _BlueJ_.");
-      
-      System.out.println( q2 );
-      System.out.print("Your answer: ");
-      String response2 = in.nextLine();
-      System.out.println(q2.checkAnswer(response2));
-      
-      ChoiceQuestion q3 = new ChoiceQuestion("Who founded Apple?");
-      q3.addChoice("Bill Gates", false);
-      q3.addChoice("Steve Jobs", true);
-      
-      System.out.println( q3.toString() );
-      System.out.print("Your answer: ");
-      String response3 = in.nextLine();
-      System.out.println(q3.checkAnswer(response3));
-      
-      
-      
-   }
+        Exam exam = new Exam();
+        exam.addQuestion(q);
+        exam.addQuestion(q2);
+        exam.addQuestion(q3);
+        
+        exam.askQuestions();
+
+    }
 }
 
